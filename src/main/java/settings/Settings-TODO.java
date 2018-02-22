a1b0d4d1f59c72dd864b6c780d1bdfed498526dc
jgrigg2013 <jgrigg2013@gmail.com>
2018-02-22 16:29:07 -0500

    Improved documentation

--- src/main/java/settings/Settings-TODO.java
+++ src/main/java/settings/Settings-TODO.java
@@ -6,11 +6,13 @@ import java.net.InetSocketAddress;
 public class Settings {
 	/*
 	 * This url is the WAN ip address of the computer being controlled.
+	 * You will likely need to set up port forwarding on your router in order to forward the
+	 * HTTP requests to the appropriate computer running Server.java
 	 */
 	public static final String urlString = "";
     /*
-     * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
-     * Alexa Skill and put the relevant Application Ids in this Set.
+     * This Id can be found on https://developer.amazon.com/edw/home.html#/ 
+     * "Edit" the relevant Alexa Skill(s) and copy/paste the Application Id(s) in this Set.
      */
 	public static final String applicationID = "";
 }
